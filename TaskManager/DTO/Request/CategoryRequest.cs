c5caa12329cf1ffd87bc8527d50d89446f5a66b4
João Raul <joaolucas.pl151@academico.ifsul.edu.br>
2025-10-27 19:05:26 -0300

    fix: adding id to CategoryRequest dto

--- TaskManager/DTO/Request/CategoryRequest.cs
+++ TaskManager/DTO/Request/CategoryRequest.cs
@@ -4,6 +4,7 @@ namespace TaskManager.DTO.Request
 {
     public class CategoryRequest
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Color { get; set; } = "#000000";
